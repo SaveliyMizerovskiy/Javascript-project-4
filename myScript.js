6c0d3e1b91ede58879b58cba0c69a17159dc94b7
Saveliy Mizerovskiy <99210593+SaveliyMizerovskiy@users.noreply.github.com>
2023-01-17 19:06:24 -0500

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,27 +1,48 @@
-function init(){
-    nameForm = document.getElementById("itemName");
-    numberForm = document.getElementById("itemNumber");
-    list = document.getElementById("list");
+const list = [];
+
+function init() {
+    listOutput = document.getElementById("list");
 }
 
-function addItem(){
-    item = nameForm.value;
-    list.innerHTML += `<li>#${list.children.length} ${item}</li>`;
+function addItem() {
+    item = (document.getElementById("item")).value;
+    list.push(item);
+    console.log(list);
+    display();
 }
 
-function moveItem(){
-    number = numberForm.value;
+function removeItem() {
+    remove = (document.getElementById("remove")).value;
+    list.splice(parseInt(remove) - 1, 1);
+    display();
 }
 
-function up(){
-
+function up() {
+    move = (document.getElementById("move")).value;
+    let index = parseInt(move) - 1;
+    if (index > 0) {
+        let temp = list[index];
+        list[index] = list[index - 1];
+        list[index - 1] = temp;
+    }
+    display();
 }
 
-function down(){
-    
+function down() {
+    move = (document.getElementById("move")).value;
+    let index = parseInt(move) - 1;
+    if (index > 0) {
+        let temp = list[index];
+        list[index] = list[index + 1];
+        list[index + 1] = temp;
+    }
+    display();
 }
 
-function removeItem(){
-    number = numberForm.value;
-    list.parentNode.removeChild(number);
+function display() {
+    listOutput.innerHTML = "";
+    for (i in list) {
+        let num = parseInt(i) + 1;
+        listOutput.innerHTML += num + ". " + list[i] + "<br>";
+    }
 }
